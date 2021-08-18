074134c80668e1cb5c49024b1261efe9c0114c6b
comsy <comsy39@gmail.com>
2021-08-18 16:32:09 +0900

    first commit

---
+++ src/test/java/nr/was/WasApplicationTests.java
@@ -0,0 +1,13 @@
+package nr.was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
